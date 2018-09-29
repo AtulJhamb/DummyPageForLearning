f49c37b5427f7ba6fea53091004f63186befed7b
atul jhamb <atuljhamb1234@gmail.con>
2018-09-29 16:50:34 +0530

    Ignore testing.java

---
+++ Testing.java
@@ -0,0 +1,14 @@
+class Testing
+{	
+	static void action()
+	{
+		System.out.println("Action method");
+	}
+}
+class Progam
+{
+	public static void main(String[] args)
+	{
+			Testing.action();
+	}
+}
